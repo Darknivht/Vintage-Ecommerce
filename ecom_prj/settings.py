369b22bceab8232423bf793aacf662715223358c
unknown <mohammedauwal2007@gmail.com>
2025-07-06 21:35:08 +0100

    Images fix

--- ecom_prj/settings.py
+++ ecom_prj/settings.py
@@ -158,8 +158,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'userauths.User'
 
